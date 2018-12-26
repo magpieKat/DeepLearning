d24200a410e43c9e42eba17afcb393c31e9f6150
Weigler Ariel-Shushan <srelwg2@bm.technion.ac.il>
2018-12-26 11:27:19 +0200

    300 Epoches code

--- HW2/HW2_NewModel.py
+++ HW2/HW2_NewModel.py
@@ -230,7 +230,7 @@ def plot_graph(vec1, title1, vec2, title2, st, date):
 if __name__ == '__main__':
     # Hyper Parameters
     plt.switch_backend('agg')
-    num_epochs = 1
+    num_epochs = 300
     batch_size = 64
     learning_rate = 0.1
     device = torch.device('cuda')
