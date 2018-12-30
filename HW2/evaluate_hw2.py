0e5cb63ff13d0540bd3232d19b7b574e512afd02
matan h <matanhamra@gmail.com>
2018-12-30 15:27:54 +0200

    add evaluate_hw2.py file

---
+++ HW2/evaluate_hw2.py
@@ -0,0 +1,56 @@
+import torch
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import HW2 as hw2
+
+
+def test(loader):
+        correct = 0
+        total = 0
+        for images, labels in loader:
+            images = images.reshape(-1, 28 * 28)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum()
+
+        accuracy = 100.0 * (correct.item() / total)
+        return accuracy
+
+
+if __name__ == '__main__':
+    # load MNIST test set
+    # Hyper parameters
+    batch_size = 64
+    output_size = 10
+    device = torch.device('cuda')
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.247, 0.2434, 0.2615)),
+    ])
+    test_dataset = dsets.CIFAR10(root='./data/',
+                                 train=False,
+                                 transform=transform_test,
+                                 download=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              pin_memory=(torch.cuda.is_available()),
+                                              num_workers=0,
+                                              shuffle=False)
+
+    # load trained network
+    model = hw2.OurModel()
+    model.load_state_dict(torch.load('26-12-18 20-13-23Accu_0.9075_model.pkl'))
+
+    # return average accuracy over test set
+    num_batches = int(len(test_dataset) / batch_size)
+    acc_vec = []
+    # for i in range(num_batches):
+    test_err = test(test_loader)
+        # acc_vec.append(test_err)
+
+    print('Test Error: %.4f' % test_err)
+    print('END')
