c6f696df09d80a869c7b10571a81b796db555b20
matanhamra <matanhamra@gmail.com>
2019-01-14 11:49:33 +0200

    meow2

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -78,7 +78,7 @@ def validiate(epoch,states):
             crt = criterion(outputs, targets.reshape(-1))
             print('criterion type: ', type(crt))
             print('criterion: ', crt[0])
-            val_loss += crt(0)
+            val_loss += crt[1]
             print('| end of epoch {:3d} | valid loss {:5.2f} | '
                   'valid ppl {:8.2f}'.format(epoch, val_loss, np.exp(val_loss)))
         val_loss = val_loss/(valid_d.size(1) // seq_length)
