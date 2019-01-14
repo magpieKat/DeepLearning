6080e140b29946aa2690a54505130bfccfffc741
matanhamra <matanhamra@gmail.com>
2019-01-14 11:36:14 +0200

    Added val

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -75,7 +75,10 @@ def validiate(epoch,states):
 
 
             outputs, states = model(inputs, states)
-            val_loss += criterion(outputs, targets.reshape(-1)).item()
+            crt = criterion(outputs, targets.reshape(-1))
+            print('criterion type: ', type(crt))
+            print('criterion: ', crt)
+            val_loss += crt[0]
             print('| end of epoch {:3d} | valid loss {:5.2f} | '
                   'valid ppl {:8.2f}'.format(epoch, val_loss, np.exp(val_loss)))
         val_loss = val_loss/(valid_d.size(1) // seq_length)
