b0812c76b486ed1de52970da50641f2cabf64277
matanhamra <matanhamra@gmail.com>
2019-01-16 16:14:16 +0200

    option 5_ver7

--- HW3/HW3Op5.py
+++ HW3/HW3Op5.py
@@ -179,13 +179,13 @@ if __name__ == '__main__':
 
     # Hyper-parameters
     embed_size = 212
-    hidden_size = 66
+    hidden_size = 67
     num_layers = 2
-    num_epochs = 14
+    num_epochs = 12
     num_samples = 5  # number of words to be sampled
     batch_size = 50
     seq_length = 30
-    learning_rate = 0.0025
+    learning_rate = 0.003
 
     # Load "Penn Treebank" dataset
     corpus = Corpus()
